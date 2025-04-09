543f8b2b256eb8bd646b3b722a0569f89997ad27
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2025-04-09 14:50:14 -0300

    funcionando mover arquivos para contratos

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes import r189, qpe, spb, nfserv, municipality_code, validation, file_processor
+from app.api.routes import r189, qpe, spb, nfserv, municipality_code, validation, file_processor, file_repository
 
 app = FastAPI(
     title="Automação Finanças API",
@@ -25,3 +25,5 @@ app.include_router(nfserv.router, prefix="/backend")
 app.include_router(municipality_code.router, prefix="/backend")
 app.include_router(validation.router, prefix="/backend/validations", tags=["Validations"])
 app.include_router(file_processor.router, prefix="/backend/files", tags=["File Processor"])
+# Corrigir o prefixo - usando /backend apenas, pois o router já tem prefixo interno "/files-repository"
+app.include_router(file_repository.router, prefix="/backend", tags=["File Repository"])
